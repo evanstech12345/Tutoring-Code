909163351a490c3906788dd6239a815843cacf7b
Evan Shor <evanshor@Evans-MacBook-Air-2.local>
2023-05-27 14:58:20 -0400

    finishing the why code page

--- client/app/whycode/page.js
+++ client/app/whycode/page.js
@@ -5,8 +5,8 @@ import Link from "next/link"
 export default function about() {
   return (
     <div className={styles.main}>
-        <h1 className='about-title'>Why Code?</h1>
-        <Link href='/'>Go back to Home</Link>
+        <h1 className={styles.whycodetitle}>Why Code?</h1>
+        <p className={styles.whycodeparagraph}>Introducing coding to children can be incredibly beneficial for their development and future success. Firstly, coding nurtures problem-solving skills and critical thinking abilities, teaching children to approach challenges with a logical mindset and break them down into manageable steps. It enhances their ability to analyze, troubleshoot, and find innovative solutions. Secondly, coding fosters creativity and unleashes imagination as children learn to create their own digital projects, such as websites, games, or animations. It allows them to express their ideas in a unique and interactive way, empowering them to become creators rather than just consumers of technology. </p>
     </div>
   )
 }
